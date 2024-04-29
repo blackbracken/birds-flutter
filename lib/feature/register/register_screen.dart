74657555ee90b14631ddfb4a0b54625fe918ba45
blackbracken <me@bracken.black>
2024-04-30 08:29:23 +0900

    set app bar

--- lib/feature/register/register_screen.dart
+++ lib/feature/register/register_screen.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_birds/components/birds_app_bar_widget.dart';
+import 'package:flutter_gen/gen_l10n/app_l10n.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class RegisterScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: BirdsAppBar(
-        title: "test",
-      ),
-    );
+        appBar: BirdsAppBar(
+          title: L10n.of(context)!.register_app_bar_label,
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(left: 32, right: 32),
+          child: Column(mainAxisSize: MainAxisSize.max, children: []),
+        ));
   }
 }
