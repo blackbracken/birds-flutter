a49086e70df90a7038a48b5d867ceae684562561
blackbracken <me@bracken.black>
2024-05-02 00:10:45 +0900

    prepare simplified result

--- lib/util/functional/Result.dart
+++ lib/util/functional/Result.dart
@@ -1,32 +1,30 @@
-sealed class Result<T, E extends Exception> {
-  const Result();
+typedef Result<T> = ResultStrictly<T, Exception>;
 
-  static Result<T, Never> success<T>(T value) {
+sealed class ResultStrictly<T, E extends Exception> {
+  const ResultStrictly();
+
+  static ResultStrictly<T, Never> success<T>(T value) {
     return Success(value);
   }
 
-  static Result<Never, E> failureSpecified<E extends Exception>(E error) {
+  static ResultStrictly<Never, E> failure<E extends Exception>(E error) {
     return Failure(error);
   }
 
-  static Result<Never, Exception> failure(Exception error) {
-    return Failure(error);
-  }
-
-  Result<U, E> map<U>(U Function(T) mapping) {
+  ResultStrictly<U, E> map<U>(U Function(T) mapping) {
     Success(1);
 
     return switch (this) {
       Success(value: var v) => Success(mapping.call(v)),
       Failure(exception: var e) => Failure(e),
       _ => throw UnimplementedError("Never supported dynamic types"),
-    } as Result<U, E>;
+    } as ResultStrictly<U, E>;
   }
 
   T? getOrNull();
 }
 
-class Success<T> extends Result<T, Never> {
+class Success<T> extends ResultStrictly<T, Never> {
   const Success(this.value);
 
   final T value;
@@ -37,7 +35,7 @@ class Success<T> extends Result<T, Never> {
   }
 }
 
-class Failure<E extends Exception> extends Result<Never, E> {
+class Failure<E extends Exception> extends ResultStrictly<Never, E> {
   const Failure(this.exception);
 
   final E exception;
