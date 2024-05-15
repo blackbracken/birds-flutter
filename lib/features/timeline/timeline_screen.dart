9952e112db9037d371347fba27fda54ac4f3ea1c
blackbracken <me@bracken.black>
2024-05-15 23:33:14 +0900

    decorate fab

--- lib/features/timeline/timeline_screen.dart
+++ lib/features/timeline/timeline_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_birds/features/timeline/timeline_hook.dart';
+import 'package:flutter_birds/hooks/use_theme.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class TimelineScreen extends HookConsumerWidget {
@@ -7,13 +8,21 @@ class TimelineScreen extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = useTheme();
     final uiModel = useTimelineUiModel(ref);
 
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: uiModel.onClickedPost,
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: SizedBox(
+          width: 64,
+          height: 64,
+          child: FloatingActionButton(
+            foregroundColor: theme.colorScheme.onPrimary,
+            backgroundColor: theme.colorScheme.primary,
+            shape: const CircleBorder(),
+            elevation: 0,
+            onPressed: uiModel.onClickedPost,
+            child: const Icon(Icons.add),
+          )),
       body: const Text('welcome to timeline'),
     );
   }
